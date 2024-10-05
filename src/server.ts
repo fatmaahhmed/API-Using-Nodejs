26c731e02c736ceb71bf08beeecf79f247c3008c
fatma ahmed <fatmaahmed222003@gmail.com>
2024-10-05 20:45:38 +0300

    refactor: Update server.ts to initialize the server with database configuration, middlewares, and routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from "express";
+
+import { applyMiddlewares } from "./middlewares/routes/server";
+import { applyRoutes } from "./routes/MainRoutes/server";
+import { databaseconfig } from "./DB/config";
+import dotenv from "dotenv";
+import { prisma } from "./prisma/config";
+
 const app = express();
 const port = process.env.PORT || 3000;
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
-});
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port},,,,`);
-});
+
+// Define the async function
+async function initializeServer() {
+  try {
+    await databaseconfig();
+    // Apply middlewares and routes
+    applyMiddlewares(app);
+    // Apply routes
+    applyRoutes(app);
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+    // Handle the error appropriately, e.g., exit the process
+    process.exit(1);
+  }
+}
+
+// Immediately invoke the named async function
+initializeServer();
