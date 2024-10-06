6ffae6c7ba32d9946f285f5d5d204ec3128d2fa3
fatma ahmed <fatmaahmed222003@gmail.com>
2024-10-06 10:17:30 +0300

    refactor: Update userController to use Prisma configuration

--- src/controllers/admin/userController.ts
+++ src/controllers/admin/userController.ts
@@ -4,9 +4,9 @@ import { add, getMany, getOne, remove, update } from "../../services/CRUD";
 import { signUp } from "../auth/1-signup";
 
 type ModelName = keyof typeof Prisma.ModelName;
-const prisma = new PrismaClient();
+
 const modelName: ModelName = "user";
-export const addUser = signUp;
+export const addUser = add(modelName);
 export const updateUser = update(modelName);
 export const deleteUser = remove(modelName);
 export const getUser = getOne(modelName);
